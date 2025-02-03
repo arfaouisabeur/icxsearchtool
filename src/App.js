957339fdca26fa9e48da8559af7653945112015b
Sabeur Arfaoui <117772841+arfaouisabeur@users.noreply.github.com>
2025-02-04 00:26:06 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import Header from "./components/header/Header";
+import Icxi from "./components/icxi/Icxi";
+import Opps from "./components/opps/Opps";
+import Contact from "./components/contact/Contact";
+import Appp from "./components/appp/Appp";
+import Testi from "./components/testi/Testi";
+
 
 function App() {
   return (
-    <div className="App">
-      <h1>dddddddddd</h1>ccccc
+    <div className="app">
+      <Header />
+      <Icxi />
+      <Opps />
+      <Appp />
+      <Testi />
+      <Contact />
     </div>
   );
 }
